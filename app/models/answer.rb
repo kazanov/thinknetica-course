34c8f0ec06290699fdd61d60e06db27512b3f7c2
kazanov <bacbok@gmail.com>
2016-04-26 13:51:33 +0300

    Conflicts removed from models and specs

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,11 +1,6 @@
 class Answer < ActiveRecord::Base
-<<<<<<< HEAD
   belongs_to :question
 
   validates :question, presence: true
-=======
-  belongs_to :question, dependent: :destroy
-
->>>>>>> 62eb033b7ab98121a634f18a63c175b08b260bcb
   validates :body, presence: true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
 
-<<<<<<< HEAD
   it { should belong_to :question }
   it { should validate_presence_of :question }
-=======
-  it { should belong_to(:question).dependent(:destroy) }
->>>>>>> 62eb033b7ab98121a634f18a63c175b08b260bcb
 
   it { should have_db_index :answer_id }
 end
