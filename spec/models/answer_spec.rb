deb7e7e2a807b5832a0b03d681ca814f09c8aaa4
kazanov <bacbok@gmail.com>
2016-05-21 15:33:58 +0300

    Dependent.destroy spec fix

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :user }
   it { should have_db_index :user_id }
 
-  it { should have_many(:attachments) }
+  it { should have_many(:attachments).dependent(:destroy) }
   it { should accept_nested_attributes_for :attachments }
 
   describe 'make_best!' do
