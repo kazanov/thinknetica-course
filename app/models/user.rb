26d79f39f686c0500b8e565b1942383b7617126b
kazanov <bacbok@gmail.com>
2016-06-06 14:47:23 +0300

    Authorization model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :votes
   has_many :comments
+  has_many :authorizations
 
   def author_of?(object)
     id == object.user_id
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
   it { should have_many(:votes) }
   it { should have_many(:comments) }
 
-  describe 'check auhtor_of?' do
+  describe '#check auhtor_of?' do
     let(:user) { create(:user) }
     let(:question) { create(:question, user: user) }
     let(:answer) { create(:answer, question: question, user: user) }
@@ -19,6 +19,18 @@ RSpec.describe User, type: :model do
 
     it 'should check if user is author of answer' do
       expect(user).to be_author_of(answer)
+    end
+  end
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context 'user already has authorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
     end
   end
 end
