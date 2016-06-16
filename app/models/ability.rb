ab32999818a225eb02ad21c3bcc8b36942107f95
kazanov <bacbok@gmail.com>
2016-06-16 15:34:52 +0300

    cancan authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,40 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    @user = user
+
+    if @user
+      user.admin? ? admin_abilities : user_abilities
+    else
+      guest_abilities
+    end
+  end
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def vote_abilities
+    can [:vote_up, :vote_down], [Question, Answer] do |votable|
+      votable.votes.find_by(user_id: @user.id).nil? && !@user.author_of?(votable)
+    end
+    can :remove_vote, [Question, Answer] do |votable|
+      votable.votes.find_by(user_id: @user.id) && !@user.author_of?(votable)
+    end
+  end
+
+  def user_abilities
+    guest_abilities
+    can :create, [Question, Answer, Comment, Attachment]
+    can [:update, :destroy], [Question, Answer], user_id: @user.id
+    can :best_answer, Answer, question: { user_id: @user.id }
+    vote_abilities
   end
 end
