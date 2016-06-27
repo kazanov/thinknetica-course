ea81fa57e13395d96bb6ce9f9ffc48f829533998
kazanov <bacbok@gmail.com>
2016-06-27 12:48:18 +0300

    Shared examples votable, commentable, attachable

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,15 +6,11 @@ RSpec.describe Question, type: :model do
   it { should validate_length_of(:title).is_at_most(140) }
 
   it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:attachments).dependent(:destroy) }
-  it { should have_many(:votes).dependent(:destroy) }
 
   it { should belong_to :user }
   it { should have_db_index :user_id }
 
-  it { should accept_nested_attributes_for :attachments }
-
-  it { should have_many(:comments).dependent(:destroy) }
-
-  it_behaves_like 'votable'
+  it_behaves_like 'Votable'
+  it_behaves_like 'Attachable'
+  it_behaves_like 'Commentable'
 end
