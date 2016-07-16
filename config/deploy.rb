f2c80ce71e4fc65f84176deb821db628ba145428
kazanov <bacbok@gmail.com>
2016-07-16 17:17:56 +0300

    Capistrano install

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'qna'
+set :repo_url, 'git@github.com:kazanov/thinknetica-course.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/qna'
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/private_pub.yml', '.env')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+
+# namespace :deploy do
+#
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#
+#     end
+#   end
+#
+# end
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
